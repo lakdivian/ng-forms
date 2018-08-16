345e77a01a41bc239efbb20f67f60f9e9e75f72b
lakdivian <noorul.km@people10.com>
2018-08-16 18:34:47 +0530

    something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-forms';
+export class AppComponent implements OnInit{
+   rForm: FormGroup;
+   post: any;
+   description: string;
+   name: string;
+   Required: string = 'This field is required';
+
+   constructor(public fbuilder: FormBuilder) {
+     this.rForm = fbuilder.group({
+         'name': [null, Validators.required],
+         'description': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+         'validate': ''
+     });
+   }
+ngOnInit() {
+  this.rForm.get('validate').valueChanges.subscribe(
+    validators => {  if (validators === '1') {
+         this.rForm.get('name').setValidators([validators.required, Validators.minLength(3)]);
+        } else {
+           this.rForm.get('name').setValidators(Validators.required);
+         }
+         this.rForm.get('name').updateValueAndValidity();
+    }
+  );
+}
+
+   addPost(post) {
+     this.name = post.value.name;
+     this.description = post.value.description;
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
